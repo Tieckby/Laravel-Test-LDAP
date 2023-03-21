23530b7eac0cbb65e197743d949091d5e80a089b
tieckby <tiemokosogodogo97@gmail.com>
2023-03-21 11:21:30 +0000

    ...

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,6 +19,6 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', 'http://portal-webapp')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-type');
+            ->header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-type, X-Auth-Token');
     }
 }
