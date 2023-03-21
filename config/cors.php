b610dbb1fee530e5dd221a7920324089da36fce4
tieckby <tiemokosogodogo97@gmail.com>
2023-03-21 11:41:53 +0000

    ...

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://portal-webapp'],
 
     'allowed_origins_patterns' => [],
 
